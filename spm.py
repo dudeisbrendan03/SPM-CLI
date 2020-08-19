7249780b9dfc34483dffe88a957cb1941e06d4fa
Brendan Jennings <jbrendan70@outlook.com>
2020-08-19 17:18:01 +0100

    Arguments

---
+++ spm.py
@@ -0,0 +1,28 @@
+#/usr/bin/python3
+import json
+import sys
+import time
+
+with open('shipping.json') as f:
+  shippingData = json.load(f)
+
+
+if '-v' in sys.argv: #Version info
+    print(f"""S* Package Manager
+    Version:    {shippingData[version]}
+    Author:     {shippingData[release]}
+    Repository: {shippingData[repository]}
+    """)
+
+if '-b' in sys.argv: #Build local package index
+    print("""WARNING!
+    Rebuilding the package index will allow the installation of packages if it doesn't exist, but overwriting an existing package index will remove your ability to manage and uninstall packages installed and controller by spm""")
+    if input("Continue (y/n): ") == 'y':
+        print("Rebuilding the index.")
+        exampleIndex = {"indexDate": time.time(),"indexGenerationVersion": shippingData[release],"packages": []}
+        with open('primary.index', 'w') as f:
+            json.dump(exampleIndex, f)
+
+
+if '-u' in sys.argv: #Download entire remote index
+    print()
