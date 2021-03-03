757532f49117d1176afc547b7043e221958d6ba3
Brendan Jennings <jbrendan70@outlook.com>
2021-03-03 22:13:46 +0000

    🚧 api lib

--- lib/api.py
+++ lib/api.py
@@ -21,17 +21,17 @@ class api(object):
         from lib.cli import cli;cli=cli(verbose)
         from lib.config import config
         #self.json = getJson() - can just use requests.json
-        self.configlib = config(verbose)
-        self.config = self.configlib.getConfig()
-        print("conf "+str(self.config))
+        self.config = config(verbose)
+        self.config = self.config.getConfig()
         #if self.config == False: raise FileNotFoundError("Missing configuration")
 
         self.log = cli.verbose
 
     def getPackage(self, package, username=None,password=None):
         self.log('Getting package information')
-        self.log(f'Opening request to {self.config['baseurl']}/{package}')
-        r = self.requests.get(f'{self.config['baseurl']}/{package}')
+        self.log(f"Opening request to {self.config['baseurl']}/{package}")
+        try: r = self.requests.get(f"{self.config['baseurl']}/{package}")
+        except: raise ConnectionError("Couldn't connect to the remote")
         if not r.status_code == 200: self.log('Server did not respond with OK (200)'); return False
         
         self.log('Reading response body as JSON')
@@ -40,8 +40,8 @@ class api(object):
 
     def getIndex(self):
         self.log('Fetching remote index')
-        self.log(f'Opening request to {self.config['baseurl']}/index')
-        r = self.requests.get(f'{self.config['baseurl']}/index')
+        self.log(f"Opening request to {self.config['baseurl']}/index")
+        r = self.requests.get(f"{self.config['baseurl']}/index")
         if not r.status_code == 200: self.log('Server did not respond with OK (200)'); return False
         
         self.log('Reading response body as JSON')
