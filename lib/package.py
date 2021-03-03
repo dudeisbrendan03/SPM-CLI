f0e43a61124ef1f7f489432473219bdc0d67f54b
Brendan Jennings <jbrendan70@outlook.com>
2021-03-03 18:13:34 +0000

    🎨🚧 New libraries

---
+++ lib/package.py
@@ -0,0 +1,18 @@
+"""
+SPM-API
+
+package tools library
+
+
+"""
+
+
+
+
+
+class package(object):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def buildIndex(self):
+        print("Going to rebu")
