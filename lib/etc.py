04b88ca05663bbbba57bf3084788df8c2b67db70
Brendan Jennings <jbrendan70@outlook.com>
2021-03-03 17:00:15 +0000

    🎨✨ Reduce reused code with getJson

---
+++ lib/etc.py
@@ -0,0 +1,9 @@
+@staticmethod
+def getJson():
+    try:
+        from simplejson import json
+        jsonModule = json
+    except:
+        import json
+        jsonModule = json
+    return jsonModule
