2dcd28d8a37d368875c0645f298d2831cf34bd12
Brendan Jennings <jbrendan70@outlook.com>
2021-03-03 14:45:07 +0000

    🎨 Operating system detection and path tools

--- lib/detection.py
+++ lib/detection.py
@@ -12,19 +12,26 @@ class detection(object):
 
         #Get users paltform
         from platform import system
-        self.sysplatform = system
+        self.sysplatform = system()
         from os import name as genericplatform
         self.genericplatform = genericplatform
 
     def platform(self):
         return self.sysplatform
         
-    def systempath(self,path):
-        if path == '$HOME':
-            if self.platform == 'Linux' or self.platform == 'Darwin':
-                return '~'
-            elif self.platform == 'Windows':
-                return '%userprofile%'
+    def platformname(self):
+        return self.genericplatform
         
+    def getpath(self,path):
+        if path ==      '$HOME':
+            if self.platform() == 'Linux' or self.platform() == 'Darwin': return '~'
+            elif self.platform() == 'Windows': return '%userprofile%'
+        elif path ==    '$DISK':
+            if self.platform() == 'Linux' or self.platform() == 'Darwin': return '/'
+            elif self.platform() == 'Windows': return 'C:\\'
+        elif path ==    '$CONFIG':
+            if self.platform() == 'Linux' or self.platform() == 'Darwin': return self.getpath('$HOME')+'/.spm/'
+            elif self.platform() == 'Windows': return self.getpath('$HOME')+'\\.spm\\'
+
         return False #not implemented
                 
