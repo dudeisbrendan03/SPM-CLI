9c79d7e948c3d67c64945ec7bd0f4a31c681e08e
Brendan Jennings <jbrendan70@outlook.com>
2021-03-03 18:22:03 +0000

    🚧 Package library

--- lib/package.py
+++ lib/package.py
@@ -15,4 +15,8 @@ class package(object):
         super().__init__()
 
     def buildIndex(self):
-        print("Going to rebu")
+        print("WARNING\nRebuilding the package index will allow the installation of packages if it doesn't exist, but overwriting an existing package index will remove your ability to manage and uninstall packages installed and controller by spm")
+        input("Press ENTER to continue, destroying the current index and building a new one, or Ctrl+C to abort, leaving files untouched.")
+    
+    def getRemoteIndex(self):
+        print("Trying to download remote index...")
