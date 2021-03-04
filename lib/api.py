fe06fcb70ef788feacc42c530a897b8ab10f7e0b
Brendan Jennings <jbrendan70@outlook.com>
2021-03-04 14:48:26 +0000

    🎨 Return tuple containing API status code per package fetched

--- lib/api.py
+++ lib/api.py
@@ -30,19 +30,19 @@ class api(object):
     def getPackage(self, package, username=None,password=None):
         self.log('Getting package information')
         self.log(f"Opening request to {self.config['baseurl']}/{package}")
-        try: r = self.requests.get(f"{self.config['baseurl']}/{package}")
-        except: raise ConnectionError("Couldn't connect to the remote")
-        if not r.status_code == 200: self.log('Server did not respond with OK (200)'); return False
+        try: r = self.requests.get(f"{self.config['baseurl']}/{package}"); self.log(f"Response {r.status_code} {r.json()}")
+        except: self.log("Failed to connect"); raise ConnectionError("Couldn't connect to the remote")
+        if not r.status_code == 200: self.log('Server did not respond with OK (200)'); return (False,r.status_code)
         
         self.log('Reading response body as JSON')
         #body = self.json.loads(r.)
-        return r.json()
+        return (r.json(),r.status_code)
 
     def getIndex(self):
         self.log('Fetching remote index')
         self.log(f"Opening request to {self.config['baseurl']}/index")
         r = self.requests.get(f"{self.config['baseurl']}/index")
         if not r.status_code == 200: self.log('Server did not respond with OK (200)'); return False
-        
+
         self.log('Reading response body as JSON')
         return r.json()
