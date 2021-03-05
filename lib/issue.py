e81544b624bea10e5f151925fcf31c5afc569066
Brendan Jennings <jbrendan70@outlook.com>
2021-03-05 16:48:35 +0000

    ✨🥅 New exceptions

--- lib/issue.py
+++ lib/issue.py
@@ -9,3 +9,34 @@ class BadPackageName(Issues):
 class FailedContentDownload(Issues):
     """Failed to download required content for a requested package, therefore the installation will not be able to continue"""
     pass
+
+class FailedInstallation(Issues):
+    """The installation failed for an unknown reason"""
+    def __init__(self, msg='Installation failed for an unknown reason', error='', *args, **kwargs):
+        if error != '': super().__init__(msg+"\nPython returned the error:\n"+str(error), *args, **kwargs) 
+        else: super().__init__(msg, *args, **kwargs)
+    pass
+
+class UnsupportedOperatingSystem(FailedInstallation):
+    """The operating system you're using is not supported by this package"""
+    def __init__(self, msg="The operating system you're using is not supported by this package", *args, **kwargs):
+        super().__init__(msg, *args, **kwargs)
+    pass
+
+class FailedScriptExecution(FailedInstallation):
+    def __init__(self, msg="The installation failed while attempting to execute a script", filename='',*args, **kwargs):
+        super().__init__(msg+'The reported file was: '+filename, *args, **kwargs)
+    pass
+
+class FailedCommandExecution(FailedInstallation):
+    def __init__(self, msg="The installation failed while attempting to execute a script", command='',*args, **kwargs):
+        super().__init__(msg+'The reported command was: '+command, *args, **kwargs)
+    pass
+
+
+class IntegrityError(Issues):
+    """A file specified to be required for the installation, removal or modification of this software failed a checksum comparison"""
+    pass
+
+class UnsupportedVersion(Issues):
+    pass
