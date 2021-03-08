3335d658b7b14eff9048d3999f310df3b309feec
Brendan Jennings <jbrendan70@outlook.com>
2021-03-08 15:43:16 +0000

    🥅 If install fails, notify and stop

--- install/git-install.sh
+++ install/git-install.sh
@@ -4,8 +4,8 @@ printf "\n  ____________   _____  \n /  ___/\\____ \\ /     \\ \n \\___ \\ |  |_
 echo "Getting ready to install"
 sleep 1
 
-if ! [[ $EUID = 0 && "$(ps -o comm= | sed -n '1p')" = "su" ]]; then
-    echo "To install this you must use sudo- but not in the shell as root."
+if ! [ -n "$SUDO_USER" && $EUID = 0]; then
+    echo "To install spm you must use sudo- but not in the shell as root"
 fi
 
 # ---------------------------------------------------------------------------- #
@@ -45,7 +45,7 @@ installpackage () {
 REQUIRED_PKG="git" && installpackage
 REQUIRED_PKG="python3" && installpackage
 REQUIRED_PKG="python3-pip" && installpackage
-REQUIRED_PKG="unzip" && installpackage
+#REQUIRED_PKG="unzip" && installpackage
 
 # -------------------------------- Clone repo -------------------------------- #
 echo "Finished installing dependencies"
@@ -77,15 +77,23 @@ if ! [[ $(pwd) = */home/$(whoami)/.spm* ]]; then
 fi
 clear
 
-echo "git release- not compiled executable"
-echo "Copying over spm.py to /usr/local/bin"
-sudo cp ~/.spm/install/spm.py /usr/local/bin/spm
-echo "Copying libs over to /usr/local/bin"
-sudo cp -r ~/.spm/install/lib /usr/local/bin/lib
-echo "Copying defaults over to ~/.spm/defaults"
-cp -r ~/.spm/install/defaults ~/.spm
-echo "Deleting install cache"
-rm -rf ~/.spm/install
-echo "Setting spm as executable"
-sudo chmod +x /usr/local/bin/spm
-echo "Install completed, type 'spm' to see usage."
+install() {
+    echo "git release- not compiled executable"
+    echo "Copying over spm.py to /usr/local/bin"
+    sudo cp ~/.spm/install/spm.py /usr/local/bin/spm
+    echo "Copying libs over to /usr/local/bin"
+    sudo cp -r ~/.spm/install/lib /usr/local/bin/lib
+    echo "Copying defaults over to ~/.spm/defaults"
+    cp -r ~/.spm/install/defaults ~/.spm
+    echo "Deleting install cache"
+    rm -rf ~/.spm/install
+    echo "Setting spm as executable"
+    sudo chmod +x /usr/local/bin/spm
+}
+
+if ! install; then
+    echo "Failed to run install process."
+    exit 1
+fi
+
+printf "\nInstall completed, type 'spm' to see usage."
